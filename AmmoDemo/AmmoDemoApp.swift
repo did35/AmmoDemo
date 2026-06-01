2c9d93d4c6128f6e65ded84c993b284a394b2a85
Didier Delhaisse <did35@yahoo.com>
2026-06-01 15:29:24 +0200

    Initial Commit

---
+++ AmmoDemo/AmmoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmmoDemoApp.swift
+//  AmmoDemo
+//
+//  Created by Didier Delhaisse on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AmmoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmmoDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AmmoDemo
+//
+//  Created by Didier Delhaisse on 01/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
